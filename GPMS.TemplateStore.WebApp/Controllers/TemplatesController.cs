04536f70364d92c2dfa3803c68445a7d105c80be
Lucas Espindola <espdlucas@gmail.com>
2017-10-01 16:18:48 -0700

    add template views

---
+++ GPMS.TemplateStore.WebApp/Controllers/TemplatesController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GPMS.TemplateStore.WebApp.Controllers
+{
+    public class TemplatesController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+    }
+}
