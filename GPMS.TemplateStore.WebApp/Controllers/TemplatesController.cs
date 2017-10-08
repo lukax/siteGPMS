b01ec9d37a1160b1cb370e2561be02cbca9d6b25
Jean Nogueira dos Santos Fontes <jean.fontes@tivit.com.br>
2017-10-08 18:03:55 -0300

    correcao bug

--- GPMS.TemplateStore.WebApp/Controllers/TemplatesController.cs
+++ GPMS.TemplateStore.WebApp/Controllers/TemplatesController.cs
@@ -8,7 +8,32 @@ namespace GPMS.TemplateStore.WebApp.Controllers
 {
     public class TemplatesController : Controller
     {
-        public IActionResult Index(int id)
+        public IActionResult Projeto(int id)
+        {
+            return View();
+        }
+		
+		public IActionResult Comunicacao(int id)
+        {
+            return View();
+        }
+		
+		public IActionResult Riscos(int id)
+        {
+            return View();
+        }
+		
+		public IActionResult Testes(int id)
+        {
+            return View();
+        }
+		
+		public IActionResult Configuracao(int id)
+        {
+            return View();
+        }
+		
+		public IActionResult Encerramento(int id)
         {
             return View();
         }
